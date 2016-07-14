78857ef4675890662e4a717edc015ef78f0f25e7
Stanley-Tian <grasslight@qq.com>
2016-07-14 15:31:27 +0800

    完成了摄像头采集

--- myIonicApp/www/js/controller.js
+++ myIonicApp/www/js/controller.js
@@ -3,11 +3,24 @@
  */
 angular.module('myApp.controllers', [])
 
+
+
   .controller('selectorController', function($scope) {
     $scope.title='请扫描户型图';
-    $scope.$on('$ionicView.afterEnter', function (viewInfo, state) {
-      selectImg();
-    });
+
+      $scope.$on('$ionicView.loaded', function (viewInfo, state) {
+        // $("#mCv").css("z-index","999");
+        // drawWindow();
+        initWebSQL();//初始化WebSQL
+        saveFeaturesToSessionStorage();//初始化SessionStorage
+      });
+      $scope.$on('$ionicView.afterEnter', function (viewInfo, state) {
+          //selectImg();\
+          initCamera();
+      });
+      $scope.$on('$ionicView.afterLeave', function (viewInfo, state) {
+          releaseCamera();
+      });
   })
   .controller('show3DController', function($scope) {
 
@@ -19,6 +32,7 @@ angular.module('myApp.controllers', [])
   })
   .controller('showDetailController', function($scope) {
     $scope.title='户型细节';
+      
     $scope.$on('$ionicView.afterEnter', function (viewInfo, state) {
       showDt();
     });
--- myIonicApp/www/js/selector.js
+++ myIonicApp/www/js/selector.js
@@ -15,3 +15,39 @@ function selectImg() {
   cans.stroke();
   //cv.textContent("aa");
 }
+function drawWindow() {
+  var c=document.getElementById("mCv");
+  var cxt=c.getContext("2d");
+  cxt.moveTo(10,10);
+  cxt.lineTo(200,10);
+  cxt.lineTo(200,200);
+  cxt.lineTo(10,200);
+  cxt.lineTo(10,10);
+  cxt.stroke();
+}
+function initCamera() {
+  var content_offset = $("#selector-content").offset();
+  var content_width = $("#selector-content").width();
+  var content_height = $("#selector-content").height();
+
+  var showed_cam ={};
+  showed_cam.width = content_width*(3/4);
+  showed_cam.height = showed_cam.width;
+  showed_cam.x = content_offset.left+(content_width - showed_cam.width)/2;
+  showed_cam.y = content_offset.top + (content_height - showed_cam.height)/2;
+
+  cordova.plugins.camerapreview.startCamera(
+      { x: showed_cam.x,
+        y: showed_cam.y,
+        width: showed_cam.width,
+        height: showed_cam.height,
+        camera: "back",
+        tapPhoto: true,
+        previewDrag: true,
+        toBack: false});
+  cordova.plugins.camerapreview.show();
+  cordova.plugins.camerapreview.focus();
+}
+function releaseCamera() {
+  cordova.plugins.camerapreview.stopCamera();
+}
