37f4d1baa4e5b156aa421b5b41a0b19e46c97066
towanglei <towanglei@163.com>
2016-07-20 08:24:22 +0800

    实现模型拆分显示功能

--- MyIonicApp3/www/js/show3D.js
+++ MyIonicApp3/www/js/show3D.js
@@ -4,6 +4,28 @@
 function show3D() {
   initControls('viewport');
   setupScene();
-  loadJsonObject('assets/HouseModles/BS01_full.json');
+  AddJsonObject('assets/HouseModles/BS_0_first.json','BS_0_first');
+  AddJsonObject('assets/HouseModles/BS_0_second.json','BS_0_second');
+  AddJsonObject('assets/HouseModles/BS_0_top.json','BS_0_top');
+  //sleep(100);
+  //HideObject('BS_0_first');
+  animate();
+}
+function show1st() {
+  ShowObject('BS_0_first');
+  HideObject('BS_0_second');
+  HideObject('BS_0_top');
+  animate();
+}
+function show2nd() {
+  ShowObject('BS_0_first');
+  ShowObject('BS_0_second');
+  HideObject('BS_0_top');
+  animate();
+}
+function showfull() {
+  ShowObject('BS_0_first');
+  ShowObject('BS_0_second');
+  ShowObject('BS_0_top');
   animate();
 }
